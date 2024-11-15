e2165f744cb73314bf1d573733883cbc787ddeae
sunainaralia <124876937+sunainaralia@users.noreply.github.com>
2024-11-15 14:02:05 +0530

    all

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import CustomErrorHandler from "./Utils/CustomErrorHandler.js";
 import ErrorHandling from "./Controllers/ErrorHandling.js";
 import { userRouter } from './Routes/UserRouter.js';
@@ -6,28 +7,43 @@ import { contactRouter } from "./Routes/ContactRouter.js";
 import { CityRouter, ProvinceRouter, ZoneRouter, PostalCodeRouter } from "./Routes/ProvinceRouter.js";
 import { uploadRouter } from "./Routes/UploadRouter.js";
 import setupSwaggerDocs from "./Middlewares/swaggerConfig.js";
+
 const app = express();
+
+// Enable CORS
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,PUT,PATCH,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+}));
+
 app.use(express.json());
+
 // Initialize Swagger Docs
 setupSwaggerDocs(app);
-// routes for user
-app.use('/api/v1/user/', userRouter);
-// routes for service request
-app.use('/api/v1/serviceReq/', contactRouter);
-// routes for province 
-app.use('/api/v1/province/', ProvinceRouter);
-// routes for city
-app.use('/api/v1/city/', CityRouter);
-// routes for zone
-app.use('/api/v1/zone/', ZoneRouter);
-// routes for postal code 
-app.use('/api/v1/postal/', PostalCodeRouter);
-// routes for upload image
-app.use('/api/v1/upload/', uploadRouter)
-app.all("*", (req, res, next) => {
-  let err = new CustomErrorHandler(`the given url ${req.originalUrl} is not present`, 400);
-  next(err)
-});
-app.use(ErrorHandling);
-export default app;
 
+// Routes for user
+app.use('/api/v1/user/', userRouter);
+// Routes for service request
+app.use('/api/v1/serviceReq/', contactRouter);
+// Routes for province 
+app.use('/api/v1/province/', ProvinceRouter);
+// Routes for city
+app.use('/api/v1/city/', CityRouter);
+// Routes for zone
+app.use('/api/v1/zone/', ZoneRouter);
+// Routes for postal code 
+app.use('/api/v1/postal/', PostalCodeRouter);
+// Routes for upload image
+app.use('/api/v1/upload/', uploadRouter);
+
+// Handle undefined routes
+app.all("*", (req, res, next) => {
+  let err = new CustomErrorHandler(`The given URL ${req.originalUrl} is not present`, 400);
+  next(err);
+});
+
+// Error handling middleware
+app.use(ErrorHandling);
+
+export default app;
